467a7322cf9e15e794176668e5e350bf0aafdeb0
kumarsai314 <62158775+kumarsai314@users.noreply.github.com>
2020-03-13 14:29:05 -0700

    Assesement 1

---
+++ factorial.py
@@ -0,0 +1,10 @@
+f=1
+x= int(input('Enter a number:'))
+if x>0 :
+    for x in range(1,x+1):
+        f= f*x
+    print('Factorial of a number is ',f)
+elif x==1 :
+        print('Factorial of a number is 1')
+elif x==0 :
+        print('Factorial of a number is not valid')
---
+++ largestone.py
@@ -0,0 +1,9 @@
+x= int(input('Enter number 1:'))
+y= int(input('Enter number 2:'))
+z= int(input('Enter number 3:'))
+if x>y>z or x>z>y:
+    print('x is largest')
+elif y>x>z or y>z>x :
+    print('y is largest')
+elif z>x>y or z>y>x :
+    print('z is largest')
